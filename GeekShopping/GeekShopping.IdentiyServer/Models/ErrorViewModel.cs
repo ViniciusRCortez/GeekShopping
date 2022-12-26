8b97f905777253e9234b013739c066ca860cac1c
Vinícius Cortêz <vinisicius2@gmail.com>
2022-12-26 15:47:21 -0300

    Ajustando erro de Nomenclatura e de referenciação de pacotes no IdentityServer

--- GeekShopping/GeekShopping.IdentiyServer/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace GeekShopping.IdentiyServer.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
